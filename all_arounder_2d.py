fc93387e6f1555b1659cd9cac4c886ebf060830d
NemanjaRanitovic <nemanjaranit@gmail.com>
2023-01-04 20:38:43 +0100

    RK4 implementirano

--- all_arounder_2d.py
+++ all_arounder_2d.py
@@ -29,37 +29,127 @@ block_top = pg.Rect(middle_blocks_left_point, 0, 250, 50)
 block_left = pg.Rect(0, sides_blocks_top_point, 50, 250)
 block_right = pg.Rect(screen_width-50, sides_blocks_top_point, 50, 250)
 
+# _____Ball_________
+# Ball diameter 
+radius = 25
+
+# Initial ball coordinates
+x = screen_width/2
+y = screen_height/2
+
+# Inital ball velocity
+vx = 150
+vy = 100
+
+# Ball acceleration 
+g = 1 # pixels / second
+
+# Ball coefficient of restitution (bounciness)
+e = 1 # MENJAJ PARAMETAR DA VIDIS KAKO UTICE NA LOPTICU  !!!
+
+# Ball mass
+mass = 1 
+
+# Coeff of friction 
+mu = 0.1
+
+# Set the ball's rolling resistance force (in Newtons)
+F_rolling_resistance = mu * mass * g  
+
+# Set the ball's acceleration (in meters/second^2)
+ax = F_rolling_resistance / mass
+ay = ax #-g
+
 # Game loop 
 running = True
 while running: 
 
-    dt = clock.tick(60)
+    #dt = clock.tick(60)
+    dt = 0.01
     screen.fill(background_color)
+
+    # RK4 Method for ball movement
+    x1, y1, vx1, vy1 = x, y, vx, vy
+    ax1, ay1 = ax, ay
+    x2, y2, vx2, vy2 = x + 0.5*vx1*dt, y + 0.5*vy1*dt, vx + 0.5*ax1*dt, vy + 0.5*ay1*dt
+    ax2, ay2 = ax, ay
+    x3, y3, vx3, vy3 = x + 0.5*vx2*dt, y + 0.5*vy2*dt, vx + 0.5*ax2*dt, vy + 0.5*ay2*dt
+    ax3, ay3 = ax, ay
+    x4, y4, vx4, vy4 = x + vx3*dt, y + vy3*dt, vx + ax3*dt, vy + ay3*dt
+    ax4, ay4 = ax, ay
+    x = x + (dt/6)*(vx1 + 2*vx2 + 2*vx3 + vx4)
+    y = y + (dt/6)*(vy1 + 2*vy2 + 2*vy3 + vy4)
+    vx = vx + (dt/6)*(ax1 + 2*ax2 + 2*ax3 + ax4)
+    vy = vy + (dt/6)*(ay1 + 2*ay2 + 2*ay3 + ay4)
+
+
+#block_bottom = pg.Rect(middle_blocks_left_point, screen_height-50, 250, 50)
+#block_top = pg.Rect(middle_blocks_left_point, 0, 250, 50)
+#block_left = pg.Rect(0, sides_blocks_top_point, 50, 250)
+#block_right = pg.Rect(screen_width-50, sides_blocks_top_point, 50, 250)
+
+    # FIRST BLOCK (top)
+    
+    if((x + radius >= 0 + middle_blocks_left_point and x + radius <= 0 + middle_blocks_left_point + 250) and (y - radius <= 50)):
+        vx = e*vx
+        vy = -e*vy
+    # SECOND BLOCK (down)
+    if((x + radius >= 0 + middle_blocks_left_point and x + radius <= 0 + middle_blocks_left_point + 250) and (y + radius >= screen_height - 50)):
+        vx = e*vx
+        vy = -e*vy
+    # THIRD BLOCK (left)
+    if((x - radius <= 50) and (y + radius >= 0 + sides_blocks_top_point and y + radius <= 0 + sides_blocks_top_point+250)): 
+        vx = -e*vx
+        vy = e*vy
+    # FOURTH BLOCK (right)
+    if ((x + radius >= screen_width-50 ) and (y + radius >= 0 + sides_blocks_top_point and y + radius <= 0 + sides_blocks_top_point+250)):
+        vx = -e*vx
+        vy = e*vy
+    
+    #Bottom border detection
+    if(y + radius >= screen_height):  
+        pg.quit()
+
+    #Top border detection
+    if(y - radius <= 0):  
+        pg.quit()
+
+    #Left border detection
+    if(x - radius <= 0):  
+        pg.quit()
+    
+    #Right border detection
+    if(x + radius >= screen_width):  
+        pg.quit()
 
     # Drawing blocks
     pg.draw.rect(screen, block_color, block_bottom, 0, 30)
     pg.draw.rect(screen, block_color, block_top, 0, 30)
     pg.draw.rect(screen, block_color, block_left, 0, 30)
     pg.draw.rect(screen, block_color, block_right, 0, 30)
+    # Drawing ball
+    pg.draw.circle(screen, ball_color, (int(x), int(y)), radius)
 
     # Block movement
     keys = pg.key.get_pressed()
-    block_movement_speed = 1.2
+    block_vertical_movement_speed = 250
+    block_horiZontal_movement_speed = 250 * 1920/1080
+
     if keys[pg.K_LEFT]:
         if(middle_blocks_left_point > 50):
-            middle_blocks_left_point -= block_movement_speed * dt
+            middle_blocks_left_point -= block_horiZontal_movement_speed * dt
 
     if keys[pg.K_RIGHT]:
         if(middle_blocks_left_point < screen.get_width()-300):
-            middle_blocks_left_point += block_movement_speed * dt 
+            middle_blocks_left_point += block_horiZontal_movement_speed * dt 
 
     if keys[pg.K_UP]:
         if(sides_blocks_top_point > 50):
-            sides_blocks_top_point -= block_movement_speed * dt
+            sides_blocks_top_point -= block_vertical_movement_speed * dt
 
     if keys[pg.K_DOWN]:
         if(sides_blocks_top_point < screen.get_height()-300):
-            sides_blocks_top_point += block_movement_speed * dt
+            sides_blocks_top_point += block_vertical_movement_speed * dt
 
     block_bottom[0] = middle_blocks_left_point
     block_top[0] = middle_blocks_left_point
